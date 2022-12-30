7506d88857691cd136b4f5864a426512f3f4025e
Danny <danny.schober@gmx.de>
2022-12-30 22:40:27 +0100

    add support for main server, ready for release 1.0

--- src/main/java/core/STATIC.java
+++ src/main/java/core/STATIC.java
@@ -4,50 +4,97 @@ import net.dv8tion.jda.api.entities.emoji.Emoji;
 
 public class STATIC {
 
+//    /* ==================================
+//                Roles for Beta
+//    ================================== */
+//    // Categories
+//    public static String CategoryGmod = "1040664314626973755";
+//    public static String CategoryPings = "1040664314626973746";
+//    public static String CategoryInterests = "1040664314593423413";
+//
+//    //Gmod
+//    public static String GLeitung = "1040664314626973754";
+//    public static String GAdmin = "1040664314626973753";
+//    public static String GSMod = "1040664314626973752";
+//    public static String GMod = "1040664314626973751";
+//    public static String GSup = "1040664314626973750";
+//    public static String GDev = "1040664314626973749";
+//    public static String GTeam = "1040664314626973748";
+//    public static String GEL = "1040664314626973747";
+//    public static String GBalancing = "1052929741314859049";
+//    public static String GCC = "1052930269025415198";
+//
+//    // Pings
+//    public static String PSpieleabend = "1040664314593423419";
+//    public static String POverwatch = "1040664314593423418";
+//    public static String PCSGO = "1040664314593423417";
+//    public static String PLeagueOfLegends = "1040664314593423416";
+//    public static String PAmongUs = "1040664314593423415";
+//    public static String PRainbow = "1040664314593423414";
+//    public static String PLOTRO = "1058479311138586664";
+//    public static String PMinecraft = "1058479385788809256";
+//    public static String PCiv6 = "1057707304159551569";
+//    public static String PHearthstone = "1058479508375736361";
+//    // Interests
+//    public static String ISpoiler = "1040664314593423412";
+//
+//    // Emojis
+//    public static Emoji Overwatch = Emoji.fromCustom("games_ow", 1057706704076284034L, false);
+//    public static Emoji CSGO = Emoji.fromCustom("games_csgo", 1057706637525258270L, false);
+//    public static Emoji LoL = Emoji.fromCustom("games_lol", 1057706673252335738L, false);
+//    public static Emoji AmongUs = Emoji.fromCustom("games_amogus", 1057706612724342895L, false);
+//    public static Emoji Rainbow = Emoji.fromCustom("games_rainbow", 1057706713014345829L, false);
+//    public static Emoji Civ6 = Emoji.fromCustom("games_civ6", 1057706622362865705L, false);
+//    public static Emoji LOTRO = Emoji.fromCustom("games_lotro", 1057706682895060993L, false);
+//    public static Emoji Minecraft = Emoji.fromCustom("games_minecraft", 1057706693024284702L, false);
+//    public static Emoji Hearthstone = Emoji.fromCustom("games_hearthstone", 1057706664322674761L, false);
+//    public static Emoji GarrysMod = Emoji.fromCustom("games_gmod", 1057706646861791253L, false);
+
+
     /* ==================================
-                Roles for Beta
+            Roles for Main
     ================================== */
     // Categories
-    public static String CategoryGmod = "1040664314626973755";
-    public static String CategoryPings = "1040664314626973746";
-    public static String CategoryInterests = "1040664314593423413";
+    public static String CategoryGmod = "1057729134635270154";
+    public static String CategoryPings = "1022501660116926486";
+    public static String CategoryInterests = "1022503601278550078";
 
     //Gmod
-    public static String GLeitung = "1040664314626973754";
-    public static String GAdmin = "1040664314626973753";
-    public static String GSMod = "1040664314626973752";
-    public static String GMod = "1040664314626973751";
-    public static String GSup = "1040664314626973750";
-    public static String GDev = "1040664314626973749";
-    public static String GTeam = "1040664314626973748";
-    public static String GEL = "1040664314626973747";
+    public static String GLeitung = "1019292167765774389";
+    public static String GAdmin = "1019292098941427823";
+    public static String GSMod = "1019291883207393391";
+    public static String GMod = "1019291796032983201";
+    public static String GSup = "1019291481724428429";
+    public static String GDev = "991765035019939870";
+    public static String GTeam = "991763971285721159";
+    public static String GEL = "1019339978200854628";
     public static String GBalancing = "1052929741314859049";
     public static String GCC = "1052930269025415198";
 
     // Pings
-    public static String PSpieleabend = "1040664314593423419";
-    public static String POverwatch = "1040664314593423418";
-    public static String PCSGO = "1040664314593423417";
-    public static String PLeagueOfLegends = "1040664314593423416";
-    public static String PAmongUs = "1040664314593423415";
-    public static String PRainbow = "1040664314593423414";
-    public static String PLOTRO = "1058479311138586664";
-    public static String PMinecraft = "1058479385788809256";
-    public static String PCiv6 = "1057707304159551569";
-    public static String PHearthstone = "1058479508375736361";
+    public static String PSpieleabend = "971104659588644934";
+    public static String POverwatch = "1022501781776900198";
+    public static String PCSGO = "1022502151047610448";
+    public static String PLeagueOfLegends = "1022502374578868225";
+    public static String PAmongUs = "1022503115523633202";
+    public static String PRainbow = "1022503155826704425";
+    public static String PLOTRO = "1057989349641297971";
+    public static String PMinecraft = "1057990669672648766";
+    public static String PCiv6 = "1057990114791411712";
+    public static String PHearthstone = "1057989407652712558";
     // Interests
-    public static String ISpoiler = "1040664314593423412";
+    public static String ISpoiler = "1022504393649705022";
 
     // Emojis
-    public static Emoji Overwatch = Emoji.fromCustom("games_ow", 1057706704076284034L, false);
-    public static Emoji CSGO = Emoji.fromCustom("games_csgo", 1057706637525258270L, false);
-    public static Emoji LoL = Emoji.fromCustom("games_lol", 1057706673252335738L, false);
-    public static Emoji AmongUs = Emoji.fromCustom("games_amogus", 1057706612724342895L, false);
-    public static Emoji Rainbow = Emoji.fromCustom("games_rainbow", 1057706713014345829L, false);
-    public static Emoji Civ6 = Emoji.fromCustom("games_civ6", 1057706622362865705L, false);
-    public static Emoji LOTRO = Emoji.fromCustom("games_lotro", 1057706682895060993L, false);
-    public static Emoji Minecraft = Emoji.fromCustom("games_minecraft", 1057706693024284702L, false);
-    public static Emoji Hearthstone = Emoji.fromCustom("games_hearthstone", 1057706664322674761L, false);
-    public static Emoji GarrysMod = Emoji.fromCustom("games_gmod", 1057706646861791253L, false);
+    public static Emoji Overwatch = Emoji.fromCustom("games_ow", 1022499872596828271L, false);
+    public static Emoji CSGO = Emoji.fromCustom("games_csgo", 1022499868251521044L, false);
+    public static Emoji LoL = Emoji.fromCustom("games_lol", 1022499870965248020L, false);
+    public static Emoji AmongUs = Emoji.fromCustom("games_amogus", 1022499866015977518L, false);
+    public static Emoji Rainbow = Emoji.fromCustom("games_rainbow", 1022499874165502032L, false);
+    public static Emoji Civ6 = Emoji.fromCustom("games_civ6", 1022499219505950791L, false);
+    public static Emoji LOTRO = Emoji.fromCustom("games_lotro", 1024367274045477034L, false);
+    public static Emoji Minecraft = Emoji.fromCustom("games_minecraft", 1024365310968602734L, false);
+    public static Emoji Hearthstone = Emoji.fromCustom("games_hearthstone", 1022503758753697823L, false);
+    public static Emoji GarrysMod = Emoji.fromCustom("games_gmod", 1024367276041977873L, false);
 
 }
